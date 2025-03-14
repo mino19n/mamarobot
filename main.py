bcdcb76d93dd6298bfea8b0f72684a13e21ae3e1
mino19n <119652981+mino19n@users.noreply.github.com>
2025-03-14 15:30:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ def webhook():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Renderの環境変数PORTを使う
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)  # デバッグモードを有効にする
