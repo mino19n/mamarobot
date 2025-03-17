bc9c40e05dcf4bb14b14302de435dd0d31ab3a71
mino19n <119652981+mino19n@users.noreply.github.com>
2025-03-17 16:00:29 +0900

    GSA修正

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 from utils import count_consecutive_days  # 祝日対応の連続日数計算
 
 # スプレッドシートのWebhook URL（GASのURL）
-SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxqDcuSzL3LRH4_RdKwnsxuYmPQl3Vy_sxzQxMDqU_h_gmgCqHX9H5JVr5L2pxTD3E4/exec"
+SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzkHPpqJMJ14ZSDEiXWoN6iUZwDZ3ahagRLSMyCVyvMxv8PGzsV0Buqyul9zr2FLr0T/exec"
 
 # スプレッドシートに記録する関数
 def send_to_sheet(user, result, streak):
